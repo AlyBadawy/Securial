a7772216eb580ba3cc33a1550790b90112ed3670
Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>
2025-06-04 16:42:59 -0400

    comment

--- lib/securial/auth.rb
+++ lib/securial/auth.rb
@@ -5,7 +5,8 @@ require "securial/auth/token_generator"
 module Securial
   module Auth
     # This is a placeholder for the Auth module.
-    # It can be used to include authentication-related methods or constants.
-    # Currently, it only includes the TokenGenerator module.
+    # It can be used to include authentication-related methods or constants
+    # that are not specific to encoding or session creation.
+    # Currently, it serves as a namespace for the AuthEncoder and SessionCreator modules.
   end
 end
