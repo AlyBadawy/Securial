7d0b940d65700f71c48c34f402bc0dbdf8b50535
Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>
2025-06-06 00:46:08 -0400

    Feat: Implement forgot password and reset password functionalities
    Signed-off-by: Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>

---
+++ app/controllers/securial/passwords_controller.rb
@@ -0,0 +1,35 @@
+module Securial
+  class PasswordsController < ApplicationController
+    skip_authentication!
+    before_action :set_user_by_password_token, only: %i[ reset_password ]
+
+    def forgot_password
+      if user = User.find_by(email_address: params.require(:email_address))
+        user.generate_reset_password_token!
+        Securial::SecurialMailer.forgot_password(user).deliver_later
+      end
+
+      render status: :ok, json: { message: "Password reset instructions sent (if user with that email address exists)." }
+    end
+
+    def reset_password
+      @user.clear_reset_password_token!
+      if @user.update(params.permit(:password, :password_confirmation))
+        render status: :ok, json: { message: "Password has been reset." }
+      else
+        render status: :unprocessable_entity, json: { errors: @user.errors }
+      end
+    end
+
+    private
+
+    def set_user_by_password_token
+      @user = User.find_by_reset_password_token!(params[:token]) # rubocop:disable Rails/DynamicFindBy
+      unless @user.reset_password_token_valid?
+        render status: :unprocessable_entity, json: { errors: { token: "is invalid or has expired" } }
+      end
+    rescue ActiveSupport::MessageVerifier::InvalidSignature, ActiveRecord::RecordNotFound
+      render status: :unprocessable_entity, json: { errors: { token: "is invalid or has expired" } } and return
+    end
+  end
+end
---
+++ spec/requests/securial/passwords_controller_spec.rb
@@ -0,0 +1,183 @@
+require 'rails_helper'
+
+RSpec.describe Securial::PasswordsController, type: :request do
+  let(:user) { create(:securial_user) }
+
+  describe "POST /forgot" do
+    it "always returns 200 OK response regardless of whether a user exists." do
+      post securial.forgot_password_url, params: { email_address: user.email_address }
+      expect(response).to be_successful
+      expect(JSON.parse(response.body)).to eq(
+        {
+          "message" => "Password reset instructions sent (if user with that email address exists).",
+        }
+      )
+    end
+
+    it "Send an email if the user exists" do
+      allow(Securial::SecurialMailer).to receive(:forgot_password).and_call_original
+      post securial.forgot_password_url, params: { email_address: user.email_address }
+      expect(Securial::SecurialMailer).to have_received(:forgot_password).with(user).exactly(1)
+    end
+
+    it "Doesn't send an email if the user doesn't exist" do
+      allow(Securial::SecurialMailer).to receive(:forgot_password).and_call_original
+      post securial.forgot_password_url, params: { email_address: "wrong@email.com" }
+      expect(Securial::SecurialMailer).not_to have_received(:forgot_password)
+    end
+  end
+
+  describe "PUT /passwords" do
+    context "when Correct token for a user is provided" do
+      it "renders a successful response" do
+        user.generate_reset_password_token!
+        token = user.reset_password_token
+        put securial.reset_password_url, params: {
+                                  token: token,
+                                  password: "New_password0",
+                                  password_confirmation: "New_password0",
+                                }
+        expect(response).to be_successful
+      end
+
+      it "resets the password when password is valid" do
+        expect(
+          Securial::User.authenticate_by(
+            email_address: user.email_address,
+            password: "New_password0",
+          )
+        ).to be_nil
+        user.generate_reset_password_token!
+        token = user.reset_password_token
+        put securial.reset_password_url, params: {
+                                  token: token,
+                                  password: "New_password0",
+                                  password_confirmation: "New_password0",
+                                }
+        expect(JSON.parse(response.body)).to eq(
+          { "message" => "Password has been reset." }
+        )
+        expect(
+          Securial::User.authenticate_by(
+            email_address: user.email_address,
+            password: "New_password0",
+          )
+        ).to eq(user)
+      end
+
+      it "doesn't reset the password when password is invalid" do
+        expect(
+          Securial::User.authenticate_by(
+            email_address: user.email_address,
+            password: "New_password0",
+          )
+        ).to be_nil
+        user.generate_reset_password_token!
+        token = user.reset_password_token
+        put securial.reset_password_url, params: {
+                                  token: token,
+                                  password: "New_password0",
+                                  password_confirmation: "incorrect",
+                                }
+        expect(JSON.parse(response.body)).to eq(
+          { "errors" => { "password_confirmation" => ["doesn't match Password"] } }
+        )
+        expect(
+          Securial::User.authenticate_by(
+            email_address: user.email_address,
+            password: "New_password0",
+          )
+        ).to be_nil
+      end
+    end
+
+    context "when Incorrect token for a user is provided" do
+      it "renders a unprocessable_entity response" do
+        put securial.reset_password_url,
+            params: {
+              token: "invalid_token",
+              password: "New_password0",
+              password_confirmation: "New_password0",
+            }
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(JSON.parse(response.body)).to eq(
+          { "errors" => { "token" => "is invalid or has expired" } }
+        )
+      end
+
+      it "Doesn't reset the password" do
+        expect(
+          Securial::User.authenticate_by(
+            email_address: user.email_address,
+            password: "Password_.1",
+          )
+        ).to eq(user)
+        put securial.reset_password_url,
+            params: {
+              token: "invalid_token",
+              password: "New_password0",
+              password_confirmation: "New_password0",
+            }
+        expect(
+          Securial::User.authenticate_by(
+            email_address: user.email_address,
+            password: "New_password0",
+          )
+        ).to be_nil
+
+        expect(
+          Securial::User.authenticate_by(
+            email_address: user.email_address,
+            password: "Password_.1",
+          )
+        ).to eq(user)
+      end
+    end
+
+    context "when token is expired" do
+      it "renders a unprocessable_entity response" do
+        user.generate_reset_password_token!
+        user.update!(reset_password_token_created_at: 3.hours.ago)
+        token = user.reset_password_token
+        put securial.reset_password_url, params: {
+                                  token: token,
+                                  password: "New_password0",
+                                  password_confirmation: "New_password0",
+                                }
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(JSON.parse(response.body)).to eq(
+          { "errors" => { "token" => "is invalid or has expired" } }
+        )
+      end
+
+      it "Doesn't reset the password" do
+        expect(
+          Securial::User.authenticate_by(
+            email_address: user.email_address,
+            password: "Password_.1",
+          )
+        ).to eq(user)
+        user.generate_reset_password_token!
+        user.update!(reset_password_token_created_at: 3.hours.ago)
+        put securial.reset_password_url, params: {
+                                  token: user.reset_password_token,
+                                  password: "New_password0",
+                                  password_confirmation: "New_password0",
+                                }
+        expect(
+          Securial::User.authenticate_by(
+            email_address: user.email_address,
+            password: "New_password0",
+          )
+        ).to be_nil
+
+        expect(
+          Securial::User.authenticate_by(
+            email_address: user.email_address,
+            password: "Password_.1",
+          )
+        ).to eq(user)
+      end
+    end
+  end
+end
