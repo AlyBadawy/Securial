d7622c667efecc5fe62813cd5909538e8f6d7fca
Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>
2025-07-10 14:56:55 -0400

    Chore: v2.1.6 (#110)

--- spec/lib/securial/securial_version_spec.rb
+++ spec/lib/securial/securial_version_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe Securial do
     end
 
     it "is the correct version" do
-      expect(version).to eq("2.1.5")
+      expect(version).to eq("2.1.6")
     end
 
     it "has three version segments" do
